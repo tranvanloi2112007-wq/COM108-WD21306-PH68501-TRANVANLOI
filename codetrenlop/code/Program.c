9729f797bc5ca5ee7fec8ad3b030d7fd775fd82f
tranvanloi2112007@gmail.com <tranvanloi2112007@gmail.com>
2025-12-01 20:09:01 +0700

    mảng và tên

--- codetrenlop/code/Program.c
+++ codetrenlop/code/Program.c
@@ -1,57 +1,91 @@
-
+#include<math.h>
 #include <stdio.h>
-void kiemTraSoNguyen()
-{ 
-    printf("kiem tr so nguyen");
-	printf("\n");
-}
-void lapChucNang(int chonChucNang)
-{
-    int tiepTuc = 1;
-    while (tiepTuc==1)
-    {
-        switch (chonChucNang)
-        {
-            case 1:
-				kiemTraSoNguyen();
-                break;
-			case 2:
-                break;
-            case 3:
-                return;
-            case 0:
-                return;
-            default:
-				printf("Chon Lai [1-3]: ");
-				
-        }
-        printf("Tiep tuc chuc nang nay? [1=co | 0=Khong");
-		scanf_s("%d", &tiepTuc);
-        system("cls");//clear
-    }
-}
+
+//void kiemTraSoNguyen()
+//{ 
+//    printf("kiem tr so nguyen");
+//	printf("\n");
+//}
+//void lapChucNang(int chonChucNang)
+//{
+//    int tiepTuc = 1;
+//    while (tiepTuc==1)
+//    {
+//        switch (chonChucNang)
+//        {
+//            case 1:
+//				kiemTraSoNguyen();
+//                break;
+//			case 2:
+//                break;
+//            case 3:
+//                return;
+//            case 0:
+//                return;
+//            default:
+//				printf("Chon Lai [1-3]: ");
+//				
+//        }
+//        printf("Tiep tuc chuc nang nay? [1=co | 0=Khong");
+//		scanf_s("%d", &tiepTuc);
+//        system("cls");//clear
+//    }
+//}
+//int main()
+//{
+//    int chonChucNang;
+//    do
+//    {
+//        pritf("Menu");
+//        printf("\n");
+//    
+//        pritf("1. kiem tra so nguyen");
+//        printf("\n");
+//    
+//        pritf("2. boi chung | uoc chung");
+//        printf("\n");
+//    
+//        pritf("Moi chon chuc nang[0-3] ");
+//        printf("\n");
+//		scanf_s("%d", &chonChucNang);
+//		lapChucNang(chonChucNang);  
+//    } while (chonChucNang != 0);
+//
+//        return 0;
+//    
+//}
+
+//int main()
+//{
+//	int n;
+//    int y[3];
+//    int tong = 0;
+//    printf("Nhap So Luong Phan Tu: \n");
+//    scanf_s("%d", &n);
+//    for (int i = 0; i < n; i++)
+//    {
+//                printf("Nhap Gia TRi Phan Tu  %d: ", i+1);
+//				scanf_s("%d", &y[i]);
+//    }
+//    
+//   
+//    for (int i = 0; i < n; i++) {
+//        printf("%d ", y[i]);
+//		tong += y[i];
+//    }
+//    
+//    
+//	printf("Tong Cac Phan Tu Trong Mang La: %d", tong);
+//    return 0;
+//}
+
 int main()
 {
-    int chonChucNang;
-    do
-    {
-        pritf("Menu");
-        printf("\n");
-    
-        pritf("1. kiem tra so nguyen");
-        printf("\n");
-    
-        pritf("2. boi chung | uoc chung");
-        printf("\n");
-    
-        pritf("Moi chon chuc nang[0-3] ");
-        printf("\n");
-		scanf_s("%d", &chonChucNang);
-		lapChucNang(chonChucNang);  
-    } while (chonChucNang != 0);
-
-        return 0;
-    
+    char ten[50];
+	printf("Nhap Ten Cua Ban: ");
+    puts(ten);
+    gets(ten);
+	printf("Ten Cua Ban La: %s",ten);
 }
 
 // Debug/Run chuong trinh: bam "F5" hoac "Debug > Start Debugging" tren menu
