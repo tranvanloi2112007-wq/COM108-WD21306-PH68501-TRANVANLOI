d213171a95eea34edeb128ecbe548528abf4ae78
tranvanloi2112007@gmail.com <tranvanloi2112007@gmail.com>
2025-12-02 17:14:37 +0700

    1 23

--- ASM/ASM/Program.c
+++ ASM/ASM/Program.c
@@ -1,24 +1,259 @@
-// CConsoleApplication.c : file nay chua 'main' function. 
-// Chuong trinh phan mem Bat dau thuc thi & Ket thuc o day.
-
 #include <stdio.h>
+#include <math.h>
+void kiemTraSoNguyen() {
+	int chon;
+	do {
+		printf("Thuc thi kiem tra so nguyen \n");
+
+		int x;
+		int i;
+		int sNT = 1;
+		int sCP = 0;
+		printf("Nhap so nguyen x: ");
+		scanf_s("%d", &x);
+		if( x != 1){    //kiem tra so nguyen
+			printf("Khong phai so nguyen\n");
+			
+		}
+		else{
+			printf("La so nguyen\n");
+		}
+		if (x <= 1) {    //ktra s ngto
+			sNT = 0;
+		}
+		else {
+			for (i = 2; i * i <= x; i++) {
+				if (x % i == 0) {
+					sNT = 0;
+					break;
+			     }
+			}
+
+		}
+		if (sNT == 1)
+			printf("%d La so nguyen to\n", x);
+		else
+			printf("%d Khong phai la so nguyen to\n", x);
+		for(i = 0; i*i <= x;i++){   //ktra scp
+			if (i * i == x) {
+				sCP = 1;
+				break;
+			}
+		}
+		if (sCP == 1) 
+			printf("%d La so chinh phuong\n", x);
+		
+		else 
+			printf("%d Khong phai la so chinh phuong\n", x);
+		
+		printf("Tiep tuc chuc nang [1|khac]: ");
+		scanf_s("%d", &chon);
+		
+	} while (chon == 1);
+}
+
+void timUocSoCHungVaBoiChungCuaHaiso() {
+	int chon;
+	do {
+		printf("Tim Uoc So Chung Va Boi Chung Cua Hai So \n");
+		int x, y;
+		int a, b;
+		printf("Nhap x: ");
+		scanf_s("%d", &x);
+		printf("Nhap y: ");	
+		scanf_s("%d", &y);
+		a = x;
+		b = y;
+		while (a != b) {
+			if (a > b)
+				a = a - b;
+			else
+				b = b - a;
+		}
+		int uocSoChungLonNhat = a;
+		int boiSoChungNhoNhat = (x * y) / uocSoChungLonNhat;
+		printf("Uoc so chung lon nhat la: %d\n", uocSoChungLonNhat);
+		printf("Boi so chung nho nhat la: %d\n",  boiSoChungNhoNhat);
+		printf("Tiep tuc chuc nang [1|khac]: ");
+		scanf_s("%d", &chon);
+	} while (chon == 1);
+}
+
+void tinhTienChoQuanKaraoke() {
+	int chon;
+	do {
+		printf(" Tinh Tien Cho Quan Karaoke \n");
+		int gioBatDau, gioKetThuc;
+		int soGio;
+		float tongTien;
+		printf("Nhap gio bat dau: ");
+		scanf_s("%d", &gioBatDau);
+		printf("Nhap gio ket thuc: ");	
+		scanf_s("%d", &gioKetThuc);
+		if (gioBatDau < 12 || gioKetThuc> 23 || gioBatDau >= gioKetThuc) {
+						printf("Khong hop le. Vui long nhap lai!\n");
+						continue;
+		}
+		soGio = gioKetThuc - gioBatDau;
+		if (soGio <= 3) {
+			tongTien = soGio * 150000;
+		}
+		else {
+			tongTien = 3 * 150000; //3h dau
+			tongTien += (soGio -3) * (150000 * 0, 7);  //h t4 gam 30%
+		}
+		if (gioBatDau >= 14 && gioBatDau <= 17) {
+			tongTien = tongTien * 0, 9;	//10%
+		}
+		printf("Tong tien can thanh toan la: %.0f\n ", tongTien);
+		printf("Tiep tuc chuc nang [1|khac]: ");
+		scanf_s("%d", &chon);
+	} while (chon == 1);
+}
+
+void tinhTienDien() {
+	int chon;
+	do {
+		printf("Tinh Tien Dien \n");
+		printf("Tiep tuc chuc nang [1|khac]: ");
+		scanf_s("%d", &chon);
+	} while (chon == 1);
+}
+
+void chucnangDoiTien() {
+	int chon;
+	do {
+		printf("Chuc Nang Doi Tien \n");
+		printf("Tiep tuc chuc nang [1|khac]: ");
+		scanf_s("%d", &chon);
+	} while (chon == 1);
+}
+
+void chucNangTinhLaiXuatVayNganHangVayTraGop() {
+	int chon;
+	do {
+		printf("Chuc Nang Tinh Lai Xuat Vay Ngan Hang Vay TRa Gop  \n");
+		printf("Tiep tuc chuc nang [1|khac]: ");
+		scanf_s("%d", &chon);
+	} while (chon == 1);
+}
+
+void vayTienMuaXe() {
+	int chon;
+	do {
+		printf("Vay Tien Mua Xe \n");
+		printf("Tiep tuc chuc nang [1|khac]: ");
+		scanf_s("%d", &chon);
+	} while (chon == 1);
+}
+
+void xapXepThongTinSinhVien() {
+	int chon;
+	do {
+		printf("Xap Xep Thong Tin Sinh Vien \n");
+		printf("Tiep tuc chuc nang [1|khac]: ");
+		scanf_s("%d", &chon);
+	} while (chon == 1);
+}
+
+void xayDungGameFPOLYLOTT() {
+	int chon;
+	do {
+		printf("Xay Dung Game \n");
+		printf("Tiep tuc chuc nang [1|khac]: ");
+		scanf_s("%d", &chon);
+	} while (chon == 1);
+}
+
+void tinhToanPhanSo() {
+	int chon;
+	do {
+		printf("Tinh Toan Phan So \n");
+		printf("Tiep tuc chuc nang [1|khac]: ");
+		scanf_s("%d", &chon);
+	} while (chon == 1);
+}
 
 int main()
 {
-    printf("Hi there!\n");
+	int chon;
+	do {
+		printf("=====Menu Chuong Trinh=====");
+		printf("\n");
+
+		printf("1. Kiem Tra So Nguyen");
+		printf("\n");
+
+		printf("2. Tim Uoc So Chung Va Boi Chung Cua Hai So");
+		printf("\n");
+
+		printf("3. Tinh Tien Cho Quan Karaoke");
+		printf("\n");
+
+		printf("4. Tinh Tien Dien");
+		printf("\n");
+
+		printf("5. Chuc Nang Doi Tien");
+		printf("\n");
+
+		printf("6. Chuc nang Tinh Lai Xuat Vay Ngan Hang Vay Tra Gop");
+		printf("\n");
+
+		printf("7. Vay Tien Mua Xe");
+		printf("\n");
+
+		printf("8. Xap Xep Thong Tin SInh Vien");
+		printf("\n");
+
+		printf("9. Xay Dung Game");
+		printf("\n");
+
+		printf("10. Tinh Toan Phan So");
+		printf("\n");
+		printf("-1. Thoat chuong Trinh ");
+		printf("\n");
+		printf("=============================\n");
+		printf("Chon chuc Nang: ");
+
+		scanf_s("%d", &chon);
+		switch (chon) {
+		case 1:
+			kiemTraSoNguyen();
+			break;
+		case 2:
+			timUocSoCHungVaBoiChungCuaHaiso();
+			break;
+		case 3:
+			tinhTienChoQuanKaraoke();
+			break;
+		case 4:
+			tinhTienDien();
+			break;
+		case 5:
+			chucnangDoiTien();
+			break;
+		case 6:
+			chucNangTinhLaiXuatVayNganHangVayTraGop();
+			break;
+		case 7:
+			vayTienMuaXe();
+			break;
+		case 8:
+			xapXepThongTinSinhVien();
+			break;
+		case 9:
+			xayDungGameFPOLYLOTT();
+			break;
+		case 10:
+			tinhToanPhanSo();
+			break;
+
+		default:
+			printf("Hay chon lai tu cn [1-10]");
+			break;
+		}
+
+	} while (chon != -1);
+
 }
 
-// Debug/Run chuong trinh: bam "F5" hoac "Debug > Start Debugging" tren menu
-
-// Danh cho nguoi moi bat dau:
-//   1. Dung "Solution Explorer window" de them/quan ly cac files
-//   2. Dung "Output window" de kiem tra "build output" va "cac thong bao khac"
-//   3. Dung "Error List window" de xem loi
-//   4. Chon "Project > Add New Item" de tao moi file code,
-//      hoac "Project > Add Existing Item" de them file da ton tai vao project nay.
-//   5. Sau nay, de mo lai project nay,
-//      chon "File > Open > Project" va chon file *.sln
-//      Co the xem thu muc project, noi luu file *.sln bang cach
-//      bam phim chuot phai vao "Solution" > chon "Open Folder in File Explorer"
-
-// GV: AnhTT184
